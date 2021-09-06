ff72734c31d27cefd8707efb11e3b2ce91f7f030
Minhong <lmh4861@gamil.com>
2021-09-06 09:16:06 +0900

    2021년 9월 6일 test

---
+++ src/main/java/com/cos/test20210906/Test20210906Application.java
@@ -0,0 +1,13 @@
+package com.cos.test20210906;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test20210906Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Test20210906Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/cos/test20210906/Test20210906ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cos.test20210906;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Test20210906ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
